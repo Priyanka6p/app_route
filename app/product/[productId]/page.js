93e1b95ede4b0a8481e8d08bcc32f10ffdfab232
Priyanka <133139652+Priyanka6p@users.noreply.github.com>
2024-05-09 17:35:59 +0530

    App Route in Next JS

---
+++ app/product/[productId]/page.js
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+
+const ProductId = ({params}) => {
+    console.log(params.productId);
+  return (
+    <div>ProductId</div>
+  )
+}
+
+export default ProductId
---
+++ app/product/page.tsx
@@ -0,0 +1,9 @@
+import exp from 'constants'
+import React from 'react'
+
+ const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+export default Product;
